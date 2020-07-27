08f32ce3c71ada8f13ee9b7cbed0186bddb14b45
mirzi <60480862+mirzi1@users.noreply.github.com>
2020-07-27 15:53:39 +0200

    log

--- scripts/vguivis.m
+++ scripts/vguivis.m
@@ -24,8 +24,33 @@ System.onScriptUnloading() {
 }
 
 beatTimer.onTimer() {
-  double beatValueL = System.getLeftVuMeter() / 255;
-  double beatValueR = System.getRightVuMeter() / 255;
+  double sens = 1.0;
+
+  double vuLeft = System.getLeftVuMeter()*sens;
+  double vuRight = System.getRightVuMeter()*sens;
+
+  //untested shit
+  /*
+  //exponential
+  double beatValueL = pow(2.718,vuLeft/46);
+  double beatValueL = pow(2.718,vuRight/46);
+  
+  //hybrid (exp+lin)
+  double beatValueL = vuLeft*vuLeft / 255;
+  double beatValueR = vuRight*vuRight / 255;
+
+  //linear
+  double beatValueL = vuLeft / 255;
+  double beatValueR = vuRight / 255;
+
+  //hybrid (log+lin)
+  double beatValueL = (sqrt(vuLeft)*15.97)) / 255;
+  double beatValueR = (sqrt(vuRight)*15.97)) / 255;
+  */
+
+  //logarithmic
+  double beatValueL = (sqrt(sqrt(vuLeft))*63.813) / 255;
+  double beatValueR = (sqrt(sqrt(vuRight))*63.813) / 255;
 
   int VGUIFrameL = beatValueL * (totalFramesL + 1);
   int VGUIFrameR = beatValueR * (totalFramesR + 1);
