c0da068eb6bb41a53621389d605630e672f65905
Gordon Freeman <38136789+0x5066@users.noreply.github.com>
2021-03-31 19:55:53 +0200

    There he lies still, and evermore, in silent suffering.

---
+++ scripts/doomguy.m
@@ -0,0 +1,46 @@
+#include "lib/std.mi"
+
+Global group doomguy;
+Global layer statusface;
+Global Timer randomint;
+Global Timer visint;
+Global int rand;
+Global int intvis;
+Global text debug1, debug2;
+
+Function doomgrin();
+
+System.onScriptLoaded(){
+
+    doomguy = getScriptGroup();
+    statusface = doomguy.getObject("st");
+    debug1 = doomguy.getObject("intvisdebug");
+    debug2 = doomguy.getObject("randdebug");
+
+    randomint = new Timer;
+    randomint.setDelay(500);
+    randomint.start();
+
+    visint = new Timer;
+    visint.setDelay(50);
+    visint.start();
+}
+
+randomint.onTimer(){
+    rand = System.random(3);
+    debug2.setXmlParam("text", System.integerToString(rand));
+}
+visint.onTimer(){
+    intvis = System.getVisBand(0,0);
+    debug1.setXmlParam("text", System.integerToString(intvis));
+
+    if(intvis > 128){
+    statusface.setXmlParam("image", "stface4");
+    }
+        else if(intvis >= 96){
+        statusface.setXmlParam("image", "stface3");
+        }
+        else{
+        statusface.setXmlParam("image", "stface"+System.integerToString(rand));
+    }
+}
