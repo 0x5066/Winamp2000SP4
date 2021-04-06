5097610fc4a0691aa58402cc3c3088845391df24
Gordon Freeman <38136789+0x5066@users.noreply.github.com>
2021-04-06 12:36:25 +0200

    cat and satsuki update

--- scripts/BPM.m
+++ scripts/BPM.m
@@ -2,7 +2,7 @@
 
 Global group doomguy;
 Global GuiObject statusface;
-Global AnimatedLayer satsukianim;
+Global AnimatedLayer satsukianim, newcat;
 Global Timer randomint;
 Global Timer visint;
 Global int rand;
@@ -21,6 +21,7 @@ System.onScriptLoaded(){
     doomguy = getScriptGroup();
     statusface = doomguy.getObject("st");
     satsukianim = doomguy.getObject("satsuki");
+    newcat = doomguy.getObject("newcat");
     debug1 = doomguy.getObject("intvisdebug");
     debug2 = doomguy.getObject("randdebug");
 
@@ -33,12 +34,17 @@ randomint.onTimer(){
     bpm = System.getPosition(); //60bpm
     int highscore = bpm*1.834/125; //110,04bpm, highscore's BPM is actually 110bpm or so but i cant get the exact number
     int highscorerestart = highscore%8; //resets the frame counter after surpassing 8, works best with animatedlayers that have "start="0" end="7"" atm
-    debug1.setXmlParam("text", System.integerToString(highscore));
-    debug2.setXmlParam("text", System.integerToString(highscorerestart));
 
-    satsukianim.gotoFrame(highscorerestart);
+    int jogging_naked = bpm*3.12985/125;
+    int jogging_nakedrestart = jogging_naked%8;
+    int jogging_nakedrestart2 = jogging_naked*2%16;
+    debug1.setXmlParam("text", System.integerToString(jogging_naked));
+    debug2.setXmlParam("text", System.integerToString(jogging_nakedrestart));
 
-    if(highscore > 0 && (highscore % 2) == 0){
+    satsukianim.gotoFrame(jogging_nakedrestart);
+    newcat.gotoFrame(jogging_nakedrestart2);
+
+    if(jogging_naked > 0 && (jogging_naked % 2) == 0){
         doomgrin();
     }
     else{
